dd2cfdf7ae06d779d3c79a94660fef93c39f9629
camokatka <elizaveta.serykh@gmail.com>
2023-08-10 11:53:45 +0300

    Initial Commit

---
+++ 3month_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3month_test
+//
+//  Created by Elizabeth Serykh on 10.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
